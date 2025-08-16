740245faad3ab6280a8542942ae77b1dfa5b848a
hugo <carvalhohugo425@gmail.com>
2025-08-16 19:26:21 -0300

    adicionar resolução dos exercicios

--- src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex1/ExercicioFacilController.java
@@ -1,13 +1,28 @@
 package school.sptech.sprint1_nota1.ex1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class ExercicioFacilController {
 
     @GetMapping("/ex-01/{palavra}")
-    public Boolean exercicioFacil(String palavra) {
-        return null;
+    public Boolean exercicioFacil(@PathVariable String palavra) {
+        String palavraMinuscula = palavra.toLowerCase();
+
+        Integer tamanhoPalavra = palavra.length() - 1;
+        Boolean palindromo = true;
+        Integer indiceFinal = tamanhoPalavra;
+        for(int i = 0; i < tamanhoPalavra / 2; i++){
+            if(palavraMinuscula.charAt(i) != palavraMinuscula.charAt(indiceFinal)){
+                palindromo = false;
+            }
+            indiceFinal--;
+        }
+
+        return palindromo;
     }
 }
--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -4,11 +4,48 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class ExercicioDificilController {
 
     @GetMapping("/ex-03/{n}")
-    public ExercicioDificilResponse exercicioDificil(int n) {
-        return null;
+    public ExercicioDificilResponse exercicioDificil(@PathVariable int n) {
+        if(n <0){
+            n = 0;
+        }
+        Integer fibonnaci = n;
+        if(n >= 4){
+            fibonnaci = fibonnaci(n);
+        }
+
+        Integer soma = 0;
+
+        for(int i = 1; i <= n;i++){
+            soma += fibonnaci(i);
+        }
+
+        ExercicioDificilResponse response = new ExercicioDificilResponse(
+                fibonnaci,
+                soma
+
+        );
+        return response;
     }
+
+    public Integer fibonnaci(Integer termo){
+        if( termo == 1){
+            return 1;
+        }else if(termo == 0){
+            return 0;
+        }
+
+        Integer f1 = fibonnaci(termo-1);
+        Integer f2 = fibonnaci(termo -2);
+
+        return f1+f2;
+    }
+
+
 }
